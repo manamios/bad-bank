e451c74ed79fbdbc65cb8e3ed9a7c3a3e975bbec
manamios <manami.osanai@gmail.com>
2022-06-28 00:34:51 -0400

    Added deposit functionality

--- deposit.js
+++ deposit.js
@@ -1,5 +1,50 @@
 function Deposit(){
+  const [balance, setBalance]                     = React.useState(100);
+  const [depositAmount, setDepositAmount]         = React.useState('');
+  const [status, setStatus]                       = React.useState('');
+  
+  function depositMoney() {
+    if (!depositAmount) {
+      setStatus('Enter deposit amount')
+      setTimeout(() => setStatus(''),3000);
+      return
+    }
+
+    if (isNaN(parseInt(depositAmount))) {
+      setStatus('Enter a number')
+      setDepositAmount('')
+      setTimeout(() => setStatus(''),3000);
+      return
+    }
+
+    if (depositAmount < 0) {
+      setStatus('Enter a positive number')
+      setDepositAmount('')
+      setTimeout(() => setStatus(''),3000);
+      return
+    }
+
+    let newBalance = balance + parseInt(depositAmount)
+
+    setBalance(newBalance)
+    setStatus(`Success! your new balance is $${newBalance}`)
+    setDepositAmount('')
+    setTimeout(() => setStatus(''),3000);
+  }
+
   return (
-    <h1>Deposit</h1>
+    <Card
+      bgcolor="primary"
+      header="Deposit"
+      status={status}
+      body={  
+              <>
+              <h3>{`Balance: $${balance}`}</h3><hr/>
+              <h3>Deposit Amount</h3>
+              <input type="input" className="form-control" id="deposit" placeholder="Enter deposit amount" value={depositAmount} onChange={e => setDepositAmount(e.target.value)}/><br/>
+              <button disabled={!depositAmount} type="submit" className="btn btn-light" onClick={depositMoney}>Deposit</button>
+              </>
+            }
+    />
   )
 }
