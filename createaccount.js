828fbb94626cd147f935e1f8d486fbb776506079
manamios <manami.osanai@gmail.com>
2022-06-27 23:31:20 -0400

    added disbale submit button logic

--- createaccount.js
+++ createaccount.js
@@ -57,7 +57,7 @@ function CreateAccount(){
               <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
               Password<br/>
               <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
-              <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
+              <button disabled={!name && !password && !email} type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
               </>
             ):(
               <>
