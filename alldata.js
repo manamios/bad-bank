5198192c39bc7f34a911ec793f6c0145512abd07
manamios <manami.osanai@gmail.com>
2022-06-28 22:19:37 -0400

    added bootstrap card

--- alldata.js
+++ alldata.js
@@ -1,9 +1,23 @@
-function AllData(){
+function AllData() {
   const ctx = React.useContext(UserContext);
+
+  let allUserDataCards = ctx.users.map((user, i) => (
+    <div key={user.name + i} className="col">
+      <Card
+        txtcolor="black"
+        header={i + 1}
+        title={user.name}
+        body={
+          <>
+            <p>E-mail: {user.email}</p>
+            <p> Password: {user.password}</p>
+          </>
+        }
+      />
+    </div>
+  ));
+  
   return (
-    <>
-    <h5>All Data in Store</h5>
-    {JSON.stringify(ctx)}<br/>
-    </>
+    <div className="row row-cols-1 row-cols-md-3 g-4">{allUserDataCards}</div>
   );
 }
